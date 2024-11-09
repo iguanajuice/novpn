985e865220b7c528ad5ea0cdda2a7bcd466f8e1a
iguanajuice <nobody@email.com>
2024-11-09 12:21:29 -0500

    Stop service from killing dhcpcd

--- novpn-ns-setup.sh
+++ novpn-ns-setup.sh
@@ -11,4 +11,4 @@ ip link add link $PHY_IFNAME $VL_IFNAME netns novpn type macvlan
 ip -n novpn link set lo up
 ip -n novpn link set $VL_IFNAME up
 
-ip netns e novpn dhcpcd -4 --leasetime 3600
+ip netns e novpn dhcpcd -4 --leasetime 3600 $VL_IFNAME -b

003986047e4d1f7c934fdfca091aceb3e2731fb0
iguanajuice <nobody@email.com>
2024-11-08 21:20:17 -0500

    Update novpn-ns-setup.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 if [ `id -u` -ne 0 ]
 then
-	echo "This script must be run as root."
+	echo "This script must be run as root"
 	exit 1
 fi
 
--- novpn-ns-setup.sh
+++ novpn-ns-setup.sh
@@ -3,6 +3,7 @@
 PHY_IFNAME=`ip route | grep '^default' | grep -oP '(?<=dev )[^\s]+'`
 VL_IFNAME=veth
 
+ip netns del novpn
 ip netns add novpn
 
 ip link add link $PHY_IFNAME $VL_IFNAME netns novpn type macvlan
@@ -10,4 +11,4 @@ ip link add link $PHY_IFNAME $VL_IFNAME netns novpn type macvlan
 ip -n novpn link set lo up
 ip -n novpn link set $VL_IFNAME up
 
-ip netns e novpn dhcpcd -4 --leasetime 3600 $VL_IFNAME
+ip netns e novpn dhcpcd -4 --leasetime 3600

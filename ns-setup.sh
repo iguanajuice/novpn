c3d36775a2ef04252c453a3908da01fcb21bdee8
iguanajuice <nobody@email.com>
2025-05-01 22:47:44 -0400

    Update ns-setup.sh

--- ns-setup.sh
+++ ns-setup.sh
@@ -5,13 +5,13 @@ PHY_IFNAME=`ip route | grep '^default' | grep -oP '(?<=dev )[^\s]+'`
 mkdir -p /etc/netns/novpn
 touch /etc/netns/novpn/resolv.conf
 
-ip link del novpn
 ip netns del novpn
 ip netns add novpn
 
-ip link add mvlan link $PHY_IFNAME netns novpn type macvlan
+ip link del novpn
 ip link add novpn type veth peer name veth
 ip link set veth netns novpn
+ip link add mvlan link $PHY_IFNAME netns novpn type macvlan
 
 ip addr add 10.0.1.1/24 dev novpn
 ip --netns novpn addr add 10.0.1.2/24 dev veth

512f1586fbd4ec2c5aa0d349576d2004c571d4de
iguanajuice <nobody@email.com>
2025-05-05 15:19:55 -0400

    Prepend host's resolv.conf while being systemd aware

--- ns-setup.sh
+++ ns-setup.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-PHY_IFNAME=`ip route | grep '^default' | grep -oP '(?<=dev )[^\s]+'`
+while ! PHY_IFNAME=`ip route | grep '^default' | grep -oP '(?<=dev )[^\s]+'`
+do sleep 0.1; done
 
 mkdir -p /etc/netns/novpn/resolv.conf.d
 rm /etc/netns/novpn/resolv.conf.d/* 2>/dev/null
-echo 'nameserver 9.9.9.9
+[ -f /run/systemd/resolve/resolv.conf ] &&
+RESOLV=/run/systemd/resolve || RESOLV=/etc
+echo "$(grep -E '^\w' $RESOLV/resolv.conf)
+nameserver 9.9.9.9
 nameserver 149.112.112.112
 nameserver 2620:fe::fe
-nameserver 2620:fe::9' > /etc/netns/novpn/resolv.conf
+nameserver 2620:fe::9" > /etc/netns/novpn/resolv.conf
 cp /etc/netns/novpn/resolv.conf /etc/netns/novpn/resolv.conf.d/@base
 
 ip netns del novpn
@@ -16,9 +20,7 @@ ip netns add novpn
 ip link del novpn
 ip link add novpn type veth peer name veth
 ip link set veth netns novpn
-while ! ip --netns novpn link show dev mvlan
-do ip link add mvlan link $PHY_IFNAME netns novpn type macvlan
-done
+ip link add mvlan link $PHY_IFNAME netns novpn type macvlan
 
 ip addr add 10.0.1.1/24 dev novpn
 ip --netns novpn addr add 10.0.1.2/24 dev veth

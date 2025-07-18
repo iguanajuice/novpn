5325bced041df72bca677b18fa051e0e23c6e5ee
iguanajuice <nobody@email.com>
2025-07-18 10:22:44 -0400

    rename XNS interfacce, add ipv6 address to XNS, and use different ipv4 for XNS

--- if-create.sh
+++ if-create.sh
@@ -1,14 +1,17 @@
 #!/bin/sh -x
 
-IF_MVL=eth0 # "MAC virtual LAN" interface
-IF_XNS=eth1 # "cross namespace" interface
+IF_XNS=xlo # name of interface used for cross-namespace localhost networking
+XNS_PFX4=172.17.0
+XNS_PFX6=fd17
 ADDR_MAC=$(cat /etc/novpn.mac) 2>/dev/null
 
 ip link add novpn type veth peer name $IF_XNS
 ip link set $IF_XNS netns novpn
 
-ip addr add 10.0.1.1/24 dev novpn
-ip --netns novpn addr add 10.0.1.2/24 dev $IF_XNS
+ip addr add $XNS_PFX4.1/24 dev novpn
+ip addr add $XNS_PFX6::1/64 dev novpn
+ip --netns novpn addr add $XNS_PFX4.2/24 dev $IF_XNS
+ip --netns novpn addr add $XNS_PFX6::2/64 dev $IF_XNS
 
 ip link set novpn up
 ip --netns novpn link set lo up
@@ -17,6 +20,6 @@ ip --netns novpn link set $IF_XNS up
 while ! IF_PHY=`ip route | grep '^default' | grep -oP '(?<=dev )[^\s]+'`
 do sleep 0.5; done
 
-ip link add $IF_MVL link $IF_PHY $ADDR_MAC netns novpn type macvlan || exit 0
-[ $ADDR_MAC ] || ip --netns novpn -j link show dev $IF_MVL | jq -r '"addr \(.[].address)"' > /etc/novpn.mac
-ip --netns novpn link set $IF_MVL up
+ip link add eth0 link $IF_PHY $ADDR_MAC netns novpn type macvlan || exit 0
+[ $ADDR_MAC ] || ip --netns novpn -j link show dev eth0 | jq -r '"addr \(.[].address)"' > /etc/novpn.mac
+ip --netns novpn link set eth0 up
